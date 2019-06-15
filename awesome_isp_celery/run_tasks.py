79d51494c6d9714965740339b31c2188fedb3437
mxmp <bandoftoys@gmail.com>
2019-06-15 22:38:10 +0400

    Доработал таски определения модели и сохранения хоста в БД.

--- awesome_isp_celery/run_tasks.py
+++ awesome_isp_celery/run_tasks.py
@@ -1,22 +1,10 @@
 from ipaddress import ip_network
-import time
 
-# from .tasks import ping_host
+from .tasks import save_host, get_model
 
 
 def print_results(results):
     return [r.result for r in results]
-
-
-def is_switch(ip_address):
-    if get_model(ip_address) in models:
-        return True
-    else:
-        return False
-
-
-def host_update(ip_address):
-    pass
 
 
 if __name__ == '__main__':
@@ -27,11 +15,13 @@ if __name__ == '__main__':
     # инфы о модели)
     hosts = network.hosts()
     for host in hosts:
-        if is_switch(host.compressed):
-            host_update(host.compressed)
+        model = get_model.delay(host.compressed)
+        if model:
+            save_host.delay(host.compressed, model, 'ok', {})
 
     # если это действительно коммутатор, то кладем инфу о нем в БД
-    # берем все коммутаторы из БД и для каждого из них спрашиваем LLDP
+    # для всех коммутаторов нужно соорудить периодическое пингование
+    # берем все коммутаторы из БД и для каждого из них спрашиваем LLDP и кладем эту инфу в БД
 
     # hosts_to_ping = ['10.99.192.19', '10.99.192.20', '10.99.192.200']
     # results = []
--- awesome_isp_celery/tasks.py
+++ awesome_isp_celery/tasks.py
@@ -1,16 +1,16 @@
 import time
 import subprocess
 
+from pymongo import MongoClient
+from easysnmp import Session
+
 from awesome_isp_celery.celery import app
 
 
-@app.task
-def longtime_add(x, y):
-    print('[tasks] long time task begins')
-    # sleep 5 seconds
-    time.sleep(5)
-    print('[tasks] long time task finished')
-    return x + y
+# Cписок моделей
+models = ['DES-3200-28/C1',
+          'DGS-3100-24TG',
+          'DGS-1510-28L/ME']
 
 
 @app.task
@@ -18,6 +18,27 @@ def ping_host(hostname):
     return subprocess.run(['ping', '-q', '-c', '1', hostname]).returncode
 
 
-@app.task(bind=True, name='save_host', queue='minio')
-def save_host(host):
-    pass
+@app.task
+def get_model(ip_address):
+    session = Session(hostname=ip_address, community='readonly', version=2)
+    sys_descr = session.get('.1.3.6.1.2.1.1.1.0')
+    for model in models:
+        if model in sys_descr:
+            return model
+
+    sys_name = session.get('.1.3.6.1.2.1.1.5.0')
+    for model in models:
+        if model in sys_name:
+            return model
+
+
+@app.task(bind=True, name='save_host')
+def save_host(ip_address, model, status, lldp_info):
+    mongo = MongoClient('mongodb://localhost:27017')
+    hosts = mongo.awesome_isp_hosts
+    host = {"ip": ip_address,
+            "model": model,
+            "status": status,
+            "lldp_info": lldp_info}
+    host_id = hosts.insert_one(host).inserted_id
+    return host_id
