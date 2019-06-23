7e2e9c95d13203d12afc91cf1bc4a83f283f173b
mxmp <bandoftoys@gmail.com>
2019-06-23 19:28:49 +0400

    Проверка хоста на доступность переделана (теперь смотрим на Chassis ID).

--- awesome_isp_celery/tasks.py
+++ awesome_isp_celery/tasks.py
@@ -4,7 +4,7 @@ import json
 from ipaddress import ip_network
 
 from pymongo import MongoClient
-from easysnmp import Session
+from easysnmp import Session, EasySNMPTimeoutError
 from celery.utils.log import get_task_logger
 
 from worker import app
@@ -15,6 +15,11 @@ logger = get_task_logger(__name__)
 models = ['DES-3200-28/C1',
           'DGS-3100-24TG',
           'DGS-1510-28L/ME']
+
+
+def mac_bin_to_hex(inc_bin_mac_address):
+    octets = [ord(c) for c in inc_bin_mac_address]
+    return "{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}".format(*octets)
 
 
 @app.task(bind=True, name='discover_hosts')
@@ -29,6 +34,19 @@ def discover_hosts(self, networks):
 @app.task(bind=True, name='check_host')
 def check_host(self, hostname):
     logger.info(f'Check host: {hostname}')
+    session = Session(hostname=hostname, community=os.environ['READ_COMMUNITY'], version=2)
+    try:
+        local_chassis_id = session.get('.1.0.8802.1.1.2.1.3.2.0')
+    except EasySNMPTimeoutError:
+        logger.info(f'Host {hostname} is down.')
+    else:
+        save_host.s(mac_bin_to_hex(local_chassis_id.value), hostname, 'unknown', 'ok', {}).delay()
+        return
+
+
+@app.task(bind=True, name='check_model')
+def check_model(self, hostname):
+    logger.info(f'Check model: {hostname}')
     session = Session(hostname=hostname, community=os.environ['READ_COMMUNITY'], version=2)
     sys_descr = session.get('.1.3.6.1.2.1.1.1.0')
     logger.info(f'sysDescr: {sys_descr}')
@@ -45,20 +63,32 @@ def check_host(self, hostname):
             return
 
 
+@app.task(bind=True, name='get_lldp_info')
+def get_lldp_info(self, hostname):
+    logger.info(f'Getting LLDP for host: {hostname}')
+    session = Session(hostname=hostname, community=os.environ['READ_COMMUNITY'], version=2)
+    nbr_mac_addresses = session.walk('.1.0.8802.1.1.2.1.4.1.1.5')
+    for entry in nbr_mac_addresses:
+        # TODO: доделать сохранение информации в БД
+        print(mac_bin_to_hex(entry.value))
+    return
+
+
 @app.task(bind=True, name='ping_host')
 def ping_host(self, hostname):
     return subprocess.run(['ping', '-q', '-c', '1', hostname]).returncode
 
 
 @app.task(bind=True, name='save_host')
-def save_host(self, ip_address, model, status, lldp_info):
+def save_host(self, id, ip_address, model, status, lldp_nbrs):
     mongo = MongoClient(os.environ['MONGO_HOST'])
     db = mongo.awesome_isp
     hosts = db.hosts
-    hosts.find_one_and_update({'ip': ip_address},
-                              {'$set': {'status': status,
+    hosts.find_one_and_update({'id': id},
+                              {'$set': {'ip': ip_address,
+                                        'status': status,
                                         'model': model,
-                                        'lldp_info': lldp_info}},
+                                        'lldp_nbrs': lldp_nbrs}},
                               upsert=True)
 
 
@@ -70,6 +100,10 @@ def make_json(self):
     nodes = []
     links = []
     for host in hosts.find():
-        nodes.append({"id": host['ip'], "model": host['model'], "group": "switches", "radius": 2})
+        nodes.append({"id": host['id'],
+                      "ip": host['ip'],
+                      "model": host['model'],
+                      "group": "switches",
+                      "radius": 2})
     with open("/usr/share/nginx/html/graph.json", "w") as graph_file:
         json.dump({"nodes": nodes, "links": links}, graph_file)
